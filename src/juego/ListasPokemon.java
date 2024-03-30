d849e393c4520b80cd7319d187d442f5c7458bfa
SergitoGF <s.gago2003@gmail.com>
2024-03-30 15:59:35 +0100

    7-FINALIZACIÓN DEL PROGRAMA

--- src/juego/ListasPokemon.java
+++ src/juego/ListasPokemon.java
@@ -5,18 +5,32 @@ import java.util.ArrayList;
 /**
  * @author Sergio Gago Fernández
  * @version 1.0
+ * @see Pokemon
  */
 public class ListasPokemon {
     private ArrayList<Pokemon> listado;
 
+    /**
+     * Constructor de ListasPokemon
+     */
     public ListasPokemon() {
         this.listado = new ArrayList<>();
     }
 
+    /**
+     * Agrega un objeto Pokemon en la lista.
+     * @param unPokemon Objeto  de tipo Pokemon
+     * @since 1.1
+     */
     public void agregarListado(Pokemon unPokemon) {
         listado.add(unPokemon);
     }
 
+    /**
+     * Muestra la lista completa de Pokemons
+     * @deprecated remplazado por {@link #mostrarListado()}
+     * @since 1.5
+     */
     public void mostrarLista() {
         System.out.println(" ");
         System.out.println("***Lista de Pokemons***");
@@ -26,6 +40,10 @@ public class ListasPokemon {
         System.out.println(" ");
     }
 
+    /**
+     * Muestra la lista completa de Pokemons
+     * @since 1.5
+     */
     public void mostrarListado() {
         System.out.println(" ");
         System.out.println("***Lista de Pokemons***");
@@ -35,20 +53,29 @@ public class ListasPokemon {
         System.out.println(" ");
     }
 
-    public void buscarNombre(String nombre) {
-        boolean encontrado = false;
+    /**
+     * @param nombre Nombre de pokemon tipo String
+     * @return Devuelve true si se encontró el pokemon, false en caso contrario.
+     * @since 1.1
+     * @see ListasPokemon
+     */
+    public boolean buscarNombre(String nombre) {
         for (Pokemon unPokemon : listado) {
             if (unPokemon.getNombre().equalsIgnoreCase(nombre)) {
                 System.out.println("¡¡POKEMON ENCONTRADO!!");
                 System.out.println(unPokemon);
-                encontrado = true;
+                return true;
             }
         }
-        if (!encontrado){
-            System.out.println("¡¡NO SE ENCUENTRA EL POKEMON!!");
-        }
+        return false;
     }
 
+    /**
+     * Busca pokemons en la lista del tipo especificado por parámetro.
+     * @param tipo tipo de pokemon de tipo String
+     * @since 1.1
+     * @see ListasPokemon
+     */
     public void pokemonsTipo(String tipo) {
         boolean encontrado = false;
         for (Pokemon unPokemon : listado) {
--- src/juego/Pokemon.java
+++ src/juego/Pokemon.java
@@ -3,24 +3,42 @@ package juego;
 /**
  * @author Sergio Gago Fernández
  * @version 1.0
+ * @see ListasPokemon
  */
 public class Pokemon {
     private String nombre;
     private String tipo;
 
+    /**
+     * Constructor de Pokemon.
+     * @param nombre Nombre del pokemon tipo String
+     * @param tipo tipo de pokemon tipo String
+     */
     public Pokemon(String nombre, String tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
     }
 
+    /**
+     * @return Devuelve el nombre del Pokemon
+     * @since 1.1
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @return Devuelve el tipo de Pokemon
+     * @since 1.1
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * @return Devuelve una línea con todos los datos de un Pokemon.
+     * @since 1.5
+     */
     @Override
     public String toString() {
         return "Pokemon [" + "(Nombre: " + nombre + ") (Tipo: " + tipo + ")]";
